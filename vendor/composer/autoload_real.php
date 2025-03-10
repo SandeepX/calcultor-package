d16fae99da26e05f8f2e510c7af7bd6439e4c427
mr.incognito <sandeeppant024@gmail.com>
2025-03-10 11:26:50 +0545

    hot fix

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInitd0a280d8adce3142a66e033cff382fd7
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInitd0a280d8adce3142a66e033cff382fd7::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
